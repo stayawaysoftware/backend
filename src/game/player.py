478b026079326477a629d7091b7c5d2785db9862
Benjamin Lozano <benjalozano13@outlook.com>
2023-09-23 14:58:59 -0300

    Added documentation for the Player class

--- src/game/player.py
+++ src/game/player.py
@@ -1,8 +1,17 @@
 
 class Player:
-    """ Player Class that represents a player in a Match """
+    """ Player Class that represents a player in a Match 
+        Attributes:
+            id (int): The id of the player inherited from User Class id (Must be the same).
+            name (str): The name of the player inherited from User Class name (Must be the same).
+            id_game (int): The id of the player inherited from Room Class id (Must be the same).
+            role (str): Player's role in the game.
+            round_position (int): Player's position during the round.
+            alive (bool): Player's status during the game.
+            quarentined (bool): Player's status about quarentine effect.
+    """
 
-    def __init__(self, id: int, name: str, id_lobby: int, role: str, round_position: int, alive: bool=True,
+    def __init__(self, id: int, name: str, id_game: int, role: str, round_position: int, alive: bool=True,
                     quarantined: bool=False):
         """Constructor for Player class"""
         self.id = id
@@ -19,11 +28,9 @@ class Player:
         return f"""User: {self.id},{self.name},{self.id_lobby},{self.role},{self.round_position},
                     {self.alive},{self.quarantined}"""
 
-    
     def get_id(self):
         """Returns the player's id"""
-        return self.id
-    
+        return self.id    
 
     def get_name(self):
         """Returns the player's name"""
