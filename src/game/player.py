afd5e9a88e632ac31e600f39999efe72b3068235
Benjamin Lozano <benjalozano13@outlook.com>
2023-09-23 14:50:30 -0300

    Removed the setters for the IDs and status properties of the Doors

--- src/game/player.py
+++ src/game/player.py
@@ -3,7 +3,7 @@ class Player:
     """ Player Class that represents a player in a Match """
 
     def __init__(self, id: int, name: str, id_lobby: int, role: str, round_position: int, alive: bool=True,
-                    quarantined: bool=False, leftDoorBlocked: bool=False, rightDoorBlocked: bool=False):
+                    quarantined: bool=False):
         """Constructor for Player class"""
         self.id = id
         self.name = name
@@ -12,23 +12,18 @@ class Player:
         self.round_position = round_position
         self.alive = alive #Weird, why would I want this condition to be false?
         self.quarantined = quarantined
-        self.leftDoorBlocked = leftDoorBlocked
-        self.rightDoorBlocked = rightDoorBlocked
 
     
     def __str__(self):
         """String representation for Player Class"""
         return f"""User: {self.id},{self.name},{self.id_lobby},{self.role},{self.round_position},
-                    {self.alive},{self.quarantined},{self.get_leftDoorBlocked},{self.get_rightDoorBlocked}"""
+                    {self.alive},{self.quarantined}"""
 
     
     def get_id(self):
         """Returns the player's id"""
         return self.id
     
-    def set_id(self, newId: int):
-        """Sets the player's id"""
-        self.id = newId
 
     def get_name(self):
         """Returns the player's name"""
@@ -41,10 +36,6 @@ class Player:
     def get_id_lobby(self):
         """Returns the player's lobby id"""
         return self.id_lobby
-
-    def set_id_lobby(self, newLobbyId: int):
-        """Sets the player's lobby id"""
-        self.id_lobby = newLobbyId
 
     def get_role(self):
         """Returns the player's role"""
@@ -77,22 +68,6 @@ class Player:
     def set_quarentined(self,newStatus: bool):
         """Sets the player's quarentine status"""
         self.quarantined = newStatus
-
-    def get_leftDoorBlocked(self):
-        """Returns the player's left Door status"""
-        return self.leftDoorBlocked
-    
-    def set_leftDoorBlocked(self, newStatus: bool):
-        """Sets the player's left Door status"""
-        self.leftDoorBlocked = newStatus
-
-    def get_rightDoorBlocked(self):
-        """Returns the player's right Door status"""
-        return self.rightDoorBlocked
-    
-    def set_rigntDoorBlocked(self, newStatus: bool):
-        """Sets the player's right Door status"""
-        self.rightDoorBlocked = newStatus
 
 
     def DrawCard(self):
