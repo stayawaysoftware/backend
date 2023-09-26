d3f41a480845b970ee03027da604956b67bdc1b8
helcsnewsxd <cphelcsnewsxd@gmail.com>
2023-09-25 22:52:26 -0300

    SAS-96 Added some logic to check the correct call for flamethrower effect

--- src/core/game/card/effects.py
+++ src/core/game/card/effects.py
@@ -5,5 +5,6 @@ from src.core.game.game_action import GameAction
 
 def flamethrower_effect(game: Game, target: int = None) -> GameAction:
     """Flamethrower effect."""
-    assert target is not None and game.get_player_quantity() > 1
+    assert game.get_actual_phase() == "PLAY" and target is not None \
+        and game.get_actual_turn() != target and game.get_player_quantity() > 1
     return GameAction("KILL", target)
