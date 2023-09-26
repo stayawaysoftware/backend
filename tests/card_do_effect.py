2d1e5f02ef3223272822572c23ff8c18fb0330d1
helcsnewsxd <cphelcsnewsxd@gmail.com>
2023-09-25 23:02:56 -0300

    SAS-96 Added tests to check flamethrower logic and implementation

--- tests/card_do_effect.py
+++ tests/card_do_effect.py
@@ -4,6 +4,8 @@ import unittest
 
 from src.core.game.card.card import Card
 from src.core.game.game_action import GameAction
+from src.core.game.game import Game
+from src.core.game.card.effects import flamethrower_effect
 
 class SimpleTest(unittest.TestCase):
     """Simple test."""
@@ -21,6 +23,42 @@ class SimpleTest(unittest.TestCase):
         """Test do_effect method 3."""
         card = Card(3, "Card 3", "Description 3", "Category 3", lambda game, x: GameAction("Action 3", x))
         self.assertEqual(str(card.do_effect(None, None)), str(GameAction("Action 3")))
+
+class RealEffectsTest(unittest.TestCase):
+    """Real effects test, with the Stay Away cards."""
+
+    # ====================== FLAMETHROWER EFFECT ======================
+
+    def test_flamethrower_effect(self):
+        """Test flamethrower_effect method."""
+        card = Card(1, "Flamethrower", "Kill a player.", "ACTION", flamethrower_effect)
+        game = Game(1, "Game 1", [1, 2, 3], 3, True, "PLAY", 1)
+        self.assertEqual(str(card.do_effect(game, 2)), str(GameAction("KILL", 2)))
+    
+    def test_flamethrower_effect_assert1(self):
+        """Test flamethrower_effect method assertion 1 -> act_player = target."""
+        card = Card(1, "Flamethrower", "Kill a player.", "ACTION", flamethrower_effect)
+        game = Game(1, "Game 1", [1, 2, 3], 3, True, "PLAY", 1)
+        self.assertRaises(AssertionError, card.do_effect, game, 1)
+    
+    def test_flamethrower_effect_assert2(self):
+        """Test flamethrower_effect method assertion 2 -> cnt_player = 1."""
+        card = Card(1, "Flamethrower", "Kill a player.", "ACTION", flamethrower_effect)
+        game = Game(1, "Game 1", [1], 1, True, "PLAY", 1)
+        self.assertRaises(AssertionError, card.do_effect, game, 2)
+    
+    def test_flamethrower_effect_assert3(self):
+        """Test flamethrower_effect method assertion 3 -> act_phase != PLAY."""
+        card = Card(1, "Flamethrower", "Kill a player.", "ACTION", flamethrower_effect)
+        game = Game(1, "Game 1", [1, 2, 3], 3, True, "DRAW", 1)
+        self.assertRaises(AssertionError, card.do_effect, game, 2)
+    
+    def test_flamethrower_effect_assert4(self):
+        """Test flamethrower_effect method assertion 4 -> target is None."""
+        card = Card(1, "Flamethrower", "Kill a player.", "ACTION", flamethrower_effect)
+        game = Game(1, "Game 1", [1, 2, 3], 3, True, "PLAY", 2)
+        self.assertRaises(AssertionError, card.do_effect, game, None)
+        
 
 if __name__ == '__main__':
     unittest.main()
