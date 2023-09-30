ad3108f70ab339334c0e6836dae0bdb879d14be1
helcsnewsxd <helcsnewsxd@helcsnewsxd>
2023-09-30 19:23:50 -0300

    SAS-121 room schemes as before

--- src/schemas/room.py
+++ src/schemas/room.py
@@ -1,11 +1,8 @@
-"""Room schemas."""
 from pydantic import BaseModel
 from pydantic import Field
 
 
 class RoomOut(BaseModel):
-    """Room schema for output only"""
-
     id: int
     name: str = Field(max_length=30)
     host_id: int
@@ -13,6 +10,12 @@ class RoomOut(BaseModel):
     max_users: int = Field(12, ge=4, le=12)
 
     class Config:
-        """Pydantic configuration"""
+        from_attributes = True
 
+
+class UserOut(BaseModel):
+    id: int
+    username: str
+
+    class Config:
         from_attributes = True
