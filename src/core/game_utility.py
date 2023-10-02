5aa67c1773e2ff00da9c6c0fb01d029da6bec423
helcsnewsxd <helcsnewsxd@helcsnewsxd>
2023-10-02 19:29:31 -0300

    SAS-156 Added return types

--- src/core/game_utility.py
+++ src/core/game_utility.py
@@ -12,10 +12,12 @@ from deck import get_deck
 from deck import get_random_card_from_available_deck
 from deck import move_discard_cards_to_available_deck
 from effects import do_effect
+from game_action import GameAction
+from models.game import Deck
 from models.game import Player
 
 
-def first_deck_creation(id_game: int, cnt_players: int):
+def first_deck_creation(id_game: int, cnt_players: int) -> Deck:
     """Create the first deck."""
     create_deck(id_game)
     create_available_deck(id_game, get_deck(id_game))
@@ -26,7 +28,7 @@ def first_deck_creation(id_game: int, cnt_players: int):
     return get_deck(id_game)
 
 
-def draw_card_from_deck(id_game: int, player: Player):
+def draw_card_from_deck(id_game: int, player: Player) -> int:
     """Draw a card from the deck."""
     if len(get_deck(id_game).available_deck.cards) == 0:
         move_discard_cards_to_available_deck(id_game)
@@ -36,7 +38,7 @@ def draw_card_from_deck(id_game: int, player: Player):
     return card.idtype
 
 
-def discard_card(id_game: int, id_card_type: int, player: Player):
+def discard_card(id_game: int, id_card_type: int, player: Player) -> None:
     """Discard a card from player hand and add to disposable deck."""
     if player.hand.select(idtype=id_card_type).count() == 0:
         raise ValueError(f"Player with id {player.id} doesn't have this card.")
@@ -46,6 +48,8 @@ def discard_card(id_game: int, id_card_type: int, player: Player):
     add_card_to_disposable_deck(id_game, card)
 
 
-def play_card(id_game: int, id_card_type: int, target: Optional[int] = None):
+def play_card(
+    id_game: int, id_card_type: int, target: Optional[int] = None
+) -> GameAction:
     """Play a card from player hand."""
     return do_effect(id_game, id_card_type, target)
