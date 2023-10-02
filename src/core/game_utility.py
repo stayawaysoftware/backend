585e6fa19f07935010ac24f88f256aeeda072bde
helcsnewsxd <helcsnewsxd@helcsnewsxd>
2023-10-02 19:05:37 -0300

    SAS-173 Added draw card from deck function

--- src/core/game_utility.py
+++ src/core/game_utility.py
@@ -1,9 +1,13 @@
 """This module contains the functions that are used to game operation."""
+from card import add_player_to_card
 from card_creation import create_card_asociation
 from deck import create_available_deck
 from deck import create_deck
 from deck import create_disposable_deck
 from deck import get_deck
+from deck import get_random_card_from_available_deck
+from deck import move_discard_cards_to_available_deck
+from models.game import Player
 
 
 def first_deck_creation(id_game: int, cnt_players: int):
@@ -15,3 +19,13 @@ def first_deck_creation(id_game: int, cnt_players: int):
     create_card_asociation(get_deck(id_game).available_deck, cnt_players)
 
     return get_deck(id_game)
+
+
+def draw_card_from_deck(id_game: int, player: Player):
+    """Draw a card from the deck."""
+    if len(get_deck(id_game).available_deck.cards) == 0:
+        move_discard_cards_to_available_deck(id_game)
+
+    card = get_random_card_from_available_deck(id_game)
+    add_player_to_card(card.id, player)
+    return card.idtype
