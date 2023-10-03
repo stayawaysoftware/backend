a6cc3cfac05ef58c696fb0930f6cbe79736c76f5
Benjamin Lozano <benjalozano13@outlook.com>
2023-10-03 03:40:59 -0300

    Complete Turn

--- src/core/game.py
+++ src/core/game.py
@@ -7,7 +7,8 @@ from models.game import Player
 from models.room import Room
 from pony.orm import commit
 from pony.orm import db_session
-
+from typing import Optional
+from core.effects import do_effect
 
 @db_session
 def init_game(room_id: int):
@@ -31,6 +32,8 @@ def init_players(room_id: int, game: Game, deck: Deck):
         player = Player(
             name=user.username, id=user.id, round_position=i, game=game
         )
+        if i == 1:
+            gu.draw_card_from_deck(room_id, player)
         for j in range(4):
             gu.draw_card_from_deck(room_id, player)
 
@@ -39,4 +42,44 @@ def init_players(room_id: int, game: Game, deck: Deck):
     players = list(game.players)
     player = random.choice(players)
     player.role = "The Thing"
+    commit()
+
+@db_session
+def play_card(
+    game_id: int,
+    card_idtype: int,
+    current_player_id: int,
+    target_player_id: Optional[int] = None,
+    ):
+        game = Game.get(id=game_id)
+        game.current_phase = "Play"
+        commit()
+        current_player = Player.get(id=current_player_id)
+        effect = do_effect(
+            id_game=game_id, id_card_type=card_idtype, target=target_player_id
+        )
+        gu.discard_card(
+            id_game=game_id, id_card_type=card_idtype, player=current_player
+        )
+        if str(effect.get_action()) == "Kill":
+            target_player = Player.get(id=target_player_id)
+            target_player.alive = False
+            commit()        
+
+@db_session
+def calculate_next_turn(
+    game_id:int
+    ):
+    game = Game.get(id=game_id)
+    players = list(game.players)
+    current_player_position = game.current_position
+    # select as next player the next player alive in the round direction
+    next_player = None
+    next_player_position = current_player_position + 1 % len(players)
+    while next_player is None:
+        if players[next_player_position].alive:
+            next_player = players[next_player_position]
+        else:
+            next_player_position = next_player_position + 1 % len(players)
+    game.current_position = next_player_position
     commit()
--- src/routes/game.py
+++ src/routes/game.py
@@ -10,6 +10,9 @@ from pony.orm import commit
 from pony.orm import db_session
 from schemas.game import GameStatus
 from schemas.player import PlayerOut
+from core.game import play_card
+from core.game import calculate_next_turn
+from core.game_utility import draw_card_from_deck
 
 game = APIRouter(tags=["game"])
 
@@ -25,22 +28,24 @@ def get_game_status(game_id: int):
             raise HTTPException(status_code=404, detail="Game not found")
         players = Game.get(id=game_id).players
         player_list = [PlayerOut.from_player(p) for p in players]
+        the_thing_player = Player.get(role="The Thing")
+        the_thing_player_status = the_thing_player.alive
         game_status = GameStatus(
             players=player_list,
             alive_players=len(player_list),
-            the_thing_is_alive=True,
-            turn_phase="Draw",
+            the_thing_is_alive=the_thing_player_status,
+            turn_phase=Game.get(id=game_id).current_phase,
             current_turn=Game.get(id=game_id).current_position,
         )
     return game_status
 
 
 @game.put(
-    "/game/{game_id}/play_card",
+    "/game/{game_id}/play_turn",
     response_model=GameStatus,
     response_description="Updated Status after play a card",
 )
-def play_card(
+def play_turn(
     game_id: int,
     card_idtype: int,
     current_player_id: int,
@@ -50,26 +55,21 @@ def play_card(
         current_game = Game.get(id=game_id)
         if not Game.exists(id=game_id):
             raise HTTPException(status_code=404, detail="Game not found")
-        current_game.current_phase = "Play"
-        commit()
-        current_player = Player.get(id=current_player_id)
-        effect = do_effect(
-            id_game=game_id, id_card_type=card_idtype, target=target_player_id
-        )
-        gu.discard_card(
-            id_game=game_id, id_card_type=card_idtype, player=current_player
-        )
-        if str(effect.get_action()) == "Kill":
-            target_player = Player.get(id=target_player_id)
-            target_player.alive = False
-            commit()
+        play_card(game_id=game_id,card_idtype=card_idtype,current_player_id=current_player_id,target_player_id=target_player_id)
         players = Game.get(id=game_id).players
         player_list = [PlayerOut.from_player(p) for p in players]
+        calculate_next_turn(game_id=game_id)
+        next_player = Player.get(round_position=current_game.current_position)
+        draw_card_from_deck(id_game=game_id, player=next_player)
+        current_game.current_phase = "Draw"
+        commit()
+        the_thing_player = Player.get(role="The Thing")
+        the_thing_player_status = the_thing_player.alive
         game_status = GameStatus(
             players=player_list,
             alive_players=len(player_list),
-            the_thing_is_alive=True,
-            turn_phase="Draw",
+            the_thing_is_alive=the_thing_player_status,
+            turn_phase=Game.get(id=game_id).current_phase,
             current_turn=Game.get(id=game_id).current_position,
         )
     return game_status
