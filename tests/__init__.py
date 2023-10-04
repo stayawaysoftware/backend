c66ad563622bda08ea68f65df0bc9c43692ee204
Geremias Baudino <61209297+gbaudino@users.noreply.github.com>
2023-10-04 14:16:32 -0300

    SAS-193 #comment Fixed import errors that broke the tests. Added comment to not parse with flake8 certain lines of __init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -6,8 +6,18 @@ current_dir = os.path.dirname(os.path.realpath(__file__))
 src_dir = os.path.join(current_dir, "..", "src")
 sys.path.append(src_dir)
 
-# Generate the database mapping
-from models import db
 
+from core.user import *  # noqa : F401
+from core.room import *  # noqa : F401
+from core.game import *  # noqa : F401
+from models import db  # noqa : F401
+from models.room import Room  # noqa : F401
+from models.room import User  # noqa : F401
+from models.game import Game  # noqa : F401
+from models.game import Player  # noqa : F401
+from pony.orm import db_session  # noqa : F401
+
+
+# Generate the database mapping
 db.bind(provider="sqlite", filename=":memory:", create_db=True)
 db.generate_mapping(create_tables=True)
