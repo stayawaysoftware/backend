984da2c737f855ef2ad47e8d26d40434bee19008
helcsnewsxd <cphelcsnewsxd@gmail.com>
2023-10-18 14:22:34 -0300

    SAS-247 Modificado GameAction para la estructura de la DEFENSA

--- src/core/game_action.py
+++ src/core/game_action.py
@@ -8,13 +8,16 @@ class ActionType(Enum):
 
     NOTHING = 1
     KILL = 2
+    ASK_DEFENSE = 3
 
     def __str__(self) -> str:
         """Return string representation."""
         if self == ActionType.NOTHING:
-            return "Nothing"
+            return "NOTHING"
         elif self == ActionType.KILL:
-            return "Kill"
+            return "KILL"
+        elif self == ActionType.ASK_DEFENSE:
+            return "ASK_DEFENSE"
 
 
 class GameAction:
@@ -26,26 +29,33 @@ class GameAction:
             action (str): Action (Nothing, Kill).
         Optional:
             target (int): Action target (player id).
+            defense_cards (list): List of defense cards.
 
     Methods:
         Get methods:
             get_action(): Return action.
             get_target(): Return target.
+            get_defense_cards(): Return defense cards.
         Set methods:
             set_action(): Set action.
             set_target(): Set target.
+            set_defense_cards(): Set defense cards.
     """
 
-    def __init__(self, action: ActionType, target: Optional[int] = None):
+    def __init__(
+        self,
+        action: ActionType,
+        target: Optional[int] = None,
+        defense_cards: Optional[list[int]] = None,
+    ):
         """Initialize class. Argument target is optional."""
         self.action = action
         self.target = target
+        self.defense_cards = defense_cards
 
     def __str__(self) -> str:
         """Return string representation."""
-        if self.target is None:
-            return str(self.action)
-        return str(self.action) + " - " + str(self.target)
+        return f"Action: {self.action}, Target: {self.target}, Defense cards: {self.defense_cards}"
 
     def get_action(self) -> ActionType:
         """Return action."""
@@ -55,6 +65,10 @@ class GameAction:
         """Return target."""
         return self.target
 
+    def get_defense_cards(self) -> Optional[list[int]]:
+        """Return defense cards."""
+        return self.defense_cards
+
     def set_action(self, action: ActionType):
         """Set action."""
         self.action = action
@@ -62,3 +76,7 @@ class GameAction:
     def set_target(self, target: Optional[int]):
         """Set target."""
         self.target = target
+
+    def set_defense_cards(self, defense_cards: Optional[list[int]]):
+        """Set defense cards."""
+        self.defense_cards = defense_cards
--- tests/test_core_game_action.py
+++ tests/test_core_game_action.py
@@ -11,18 +11,33 @@ class TestGameAction:
         action = GameAction(ActionType.NOTHING)
         assert action.get_action() == ActionType.NOTHING
         assert action.get_target() is None
+        assert action.get_defense_cards() is None
 
         action = GameAction(ActionType.KILL, 1)
         assert action.get_action() == ActionType.KILL
         assert action.get_target() == 1
+        assert action.get_defense_cards() is None
+
+        action = GameAction(ActionType.ASK_DEFENSE, 1, [1, 2])
+        assert action.get_action() == ActionType.ASK_DEFENSE
+        assert action.get_target() == 1
+        assert action.get_defense_cards() == [1, 2]
 
     def test_str(self):
         """Test string representation."""
         action = GameAction(ActionType.NOTHING)
-        assert str(action) == "Nothing"
+        assert (
+            str(action) == "Action: NOTHING, Target: None, Defense cards: None"
+        )
 
         action = GameAction(ActionType.KILL, 1)
-        assert str(action) == "Kill - 1"
+        assert str(action) == "Action: KILL, Target: 1, Defense cards: None"
+
+        action = GameAction(ActionType.ASK_DEFENSE, 1, [1, 2])
+        assert (
+            str(action)
+            == "Action: ASK_DEFENSE, Target: 1, Defense cards: [1, 2]"  # noqa W503
+        )
 
     def test_get_action(self):
         """Test get_action()."""
@@ -32,6 +47,9 @@ class TestGameAction:
         action = GameAction(ActionType.KILL, 1)
         assert action.get_action() == ActionType.KILL
 
+        action = GameAction(ActionType.ASK_DEFENSE, 1, [1, 2])
+        assert action.get_action() == ActionType.ASK_DEFENSE
+
     def test_get_target(self):
         """Test get_target()."""
         action = GameAction(ActionType.NOTHING)
@@ -39,6 +57,20 @@ class TestGameAction:
 
         action = GameAction(ActionType.KILL, 1)
         assert action.get_target() == 1
+
+        action = GameAction(ActionType.ASK_DEFENSE, 1, [1, 2])
+        assert action.get_target() == 1
+
+    def test_get_defense_cards(self):
+        """Test get_defense_cards()."""
+        action = GameAction(ActionType.NOTHING)
+        assert action.get_defense_cards() is None
+
+        action = GameAction(ActionType.KILL, 1)
+        assert action.get_defense_cards() is None
+
+        action = GameAction(ActionType.ASK_DEFENSE, 1, [1, 2])
+        assert action.get_defense_cards() == [1, 2]
 
     def test_set_action(self):
         """Test set_action()."""
@@ -50,6 +82,10 @@ class TestGameAction:
         action.set_action(ActionType.NOTHING)
         assert action.get_action() == ActionType.NOTHING
 
+        action = GameAction(ActionType.ASK_DEFENSE, 1, [1, 2])
+        action.set_action(ActionType.KILL)
+        assert action.get_action() == ActionType.KILL
+
     def test_set_target(self):
         """Test set_target()."""
         action = GameAction(ActionType.NOTHING)
@@ -59,3 +95,21 @@ class TestGameAction:
         action = GameAction(ActionType.KILL, 1)
         action.set_target(2)
         assert action.get_target() == 2
+
+        action = GameAction(ActionType.ASK_DEFENSE, 1, [1, 2])
+        action.set_target(2)
+        assert action.get_target() == 2
+
+    def test_set_defense_cards(self):
+        """Test set_defense_cards()."""
+        action = GameAction(ActionType.NOTHING)
+        action.set_defense_cards([1, 2])
+        assert action.get_defense_cards() == [1, 2]
+
+        action = GameAction(ActionType.KILL, 1)
+        action.set_defense_cards([1, 2])
+        assert action.get_defense_cards() == [1, 2]
+
+        action = GameAction(ActionType.ASK_DEFENSE, 1, [1, 2])
+        action.set_defense_cards([3, 4])
+        assert action.get_defense_cards() == [3, 4]
