1fc53f24567d4eee3d0fc47ceb2b6a5ef9de20ef
helcsnewsxd <cphelcsnewsxd@gmail.com>
2023-10-23 11:16:56 -0300

    SAS-287 Implementado efecto de Análisis

--- src/core/effects.py
+++ src/core/effects.py
@@ -45,7 +45,7 @@ def do_effect(
             else:
                 return flamethrower_effect(id_game, target)
         case 4:  # Analysis
-            return nothing_effect(id_game)
+            return analysis_effect(id_game, id_player, target)
         case 5:  # Axe
             return nothing_effect(id_game)
         case 6:  # Suspicion
@@ -182,6 +182,31 @@ def ask_defense_effect(id_card_type: int, target: Optional[int]) -> GameAction:
             action=ActionType.ASK_DEFENSE,
             target=[target],
             defense_cards=card_defense[id_card_type],
+        )
+
+
+def analysis_effect(
+    id_game: int, id_player: int, target: Optional[int]
+) -> GameAction:
+    """Analysis effect."""
+    with db_session:
+        game = Game[id_game]
+
+        if game.current_phase != "Play":
+            raise ValueError("You can't use this card in this phase.")
+        if target is None:
+            raise ValueError("You must select a target.")
+        if game.players.select(id=target).count() == 0:
+            raise ValueError("Target doesn't exists.")
+        if not game.players.select(id=target).first().alive:
+            raise ValueError("Target is dead.")
+        if game.players.select(id=id_player).count() == 0:
+            raise ValueError("Player doesn't exists.")
+        if game.players.select(id=id_player).first().id == target:
+            raise ValueError("You can't use this card on yourself.")
+
+        return GameAction(
+            action=ActionType.SHOW_ALL, target=[target, id_player]
         )
 
 
