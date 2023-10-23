71b49631fd1db0c3ef33c205cb54169c3febf173
helcsnewsxd <cphelcsnewsxd@gmail.com>
2023-10-23 10:10:21 -0300

    SAS-282 Implementados tests del efecto de defensa Nada de Barbacoas

--- tests/test_core_effects.py
+++ tests/test_core_effects.py
@@ -478,7 +478,101 @@ class TestFlamethrowerEffectREAL:
         self.end_db()
 
 
+# ============================ FLAMETHROWER EFFECT (WITH DEFENSE) ============================
+
+
+class TestNoBarbacuesEffect:
+    """Tests for no barbacues effect."""
+
+    @classmethod
+    @db_session
+    def setup_class(cls):
+        """Setup class."""
+        clean_db()
+
+    @classmethod
+    def teardown_class(cls):
+        """Teardown class."""
+        clean_db()
+
+    def init_db(self):
+        """Init DB."""
+        with db_session:
+            Deck(id=1)
+            Game(id=1, deck=Deck[1], current_phase="Play")
+            Player(
+                id=1,
+                game=Game[1],
+                alive=True,
+                round_position=1,
+                name="Player 1",
+            )
+            Player(
+                id=2,
+                game=Game[1],
+                alive=True,
+                round_position=2,
+                name="Player 2",
+            )
+            commit()
+
+    def end_db(self):
+        """End DB."""
+        with db_session:
+            Game[1].delete()
+            Deck[1].delete()
+            Player[1].delete()
+            Player[2].delete()
+            commit()
+
+    @db_session
+    def test_no_barbacues_effect(self):
+        """Test no barbacues effect."""
+        self.init_db()
+
+        Game[1].current_phase = "Play"
+        do_effect(
+            id_game=1, id_player=1, id_card_type=3, target=2
+        )  # Play flamethrower
+
+        Game[1].current_phase = "Defense"
+        assert str(
+            do_effect(
+                id_game=1,
+                id_player=2,
+                id_card_type=17,
+                id_card_type_before=3,
+                target=1,
+            )
+        ) == str(GameAction(action=ActionType.NOTHING))
+
+        self.end_db()
+
+    @db_session
+    def test_no_barbacues_effect_in_invalid_phase(self):
+        """Test no barbacues effect in invalid phase."""
+        self.init_db()
+
+        Game[1].current_phase = "Play"
+        do_effect(
+            id_game=1, id_player=1, id_card_type=3, target=2
+        )  # Play flamethrower
+
+        Game[1].current_phase = "Play"
+        with pytest.raises(ValueError):
+            do_effect(
+                id_game=1,
+                id_player=2,
+                id_card_type=17,
+                id_card_type_before=3,
+                target=1,
+            )
+
+        self.end_db()
+
+
 # ============================ EXCHANGE EFFECT ============================
+
 
 class TestExchangeREALEFFECT:
     """Tests for exchange effect."""
