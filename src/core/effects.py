c03b34d5c3ea7bebf5dc859883e830e72c2ecd62
helcsnewsxd <cphelcsnewsxd@gmail.com>
2023-10-23 10:40:49 -0300

    SAS-284 Implementado efecto Aterrador

--- src/core/effects.py
+++ src/core/effects.py
@@ -74,7 +74,9 @@ def do_effect(
         case 13:  # I'm fine here
             return nothing_effect(id_game)
         case 14:  # Terrifying
-            return nothing_effect(id_game)
+            return terrifying_effect(
+                id_game, id_player, target, card_chosen_by_target
+            )
         case 15:  # No, thanks
             return no_thanks_effect(id_game)
         case 16:  # You failed
@@ -335,6 +337,38 @@ def exchange_effect(
                 target=[player, target, player_infected],
                 card_target=[card_chosen_by_player, card_chosen_by_target],
             )
+
+
+def terrifying_effect(
+    id_game: int,
+    player: int,
+    target: Optional[int],
+    card_chosen_by_target: Optional[int],
+) -> GameAction:
+    """Terrifying effect."""
+    with db_session:
+        game = Game[id_game]
+
+        if game.current_phase != "Defense":
+            raise ValueError("You can't use this card in this phase.")
+        if target is None:
+            raise ValueError("You must select a target.")
+        if game.players.select(id=target).count() == 0:
+            raise ValueError("Target doesn't exists.")
+        if not game.players.select(id=target).first().alive:
+            raise ValueError("Target is dead.")
+        if game.players.select(id=player).count() == 0:
+            raise ValueError("Player doesn't exists.")
+        if game.players.select(id=player).first().id == target:
+            raise ValueError("You can't use this card on yourself.")
+        if card_chosen_by_target is None:
+            raise ValueError("You must select a card before.")
+
+        return GameAction(
+            action=ActionType.SHOW,
+            target=[target, player],
+            card_target=[card_chosen_by_target],
+        )
 
 
 def no_thanks_effect(id_game: int) -> GameAction:
