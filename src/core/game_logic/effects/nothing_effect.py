b6a43878c2a3afc46fb0dad702d92c7ffef84636
helcsnewsxd <cphelcsnewsxd@gmail.com>
2023-11-05 15:50:46 -0300

    SAS-332 Arreglados imports dado que no deben contener el src. al principio

--- src/core/game_logic/effects/nothing_effect.py
+++ src/core/game_logic/effects/nothing_effect.py
@@ -1,7 +1,7 @@
 """Nothing effect."""
+from core.game_logic.game_action import ActionType
+from core.game_logic.game_action import GameAction
 from pony.orm import db_session
-from src.core.game_logic.game_action import ActionType
-from src.core.game_logic.game_action import GameAction
 
 
 def nothing_effect(id_game: int) -> GameAction:
