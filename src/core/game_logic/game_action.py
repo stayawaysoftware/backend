3b41b7ad1288d159c0b6ace38853b2ccc7f22750
helcsnewsxd <cphelcsnewsxd@gmail.com>
2023-11-05 14:18:49 -0300

    SAS-332 Realizado traslado de archivos de lógica de juego a nuevo directorio dentro de core

---
+++ src/core/game_logic/game_action.py
@@ -0,0 +1,147 @@
+"""Module providing the interconnection between the game and the card effects."""
+from enum import Enum
+from typing import Optional
+
+
+class ActionType(Enum):
+    """Enum class for action types."""
+
+    NOTHING = 1
+    ASK_DEFENSE = 2
+    ASK_EXCHANGE = 3
+
+    EXCHANGE = 4
+    INFECT = 5
+    KILL = 6
+    SHOW = 7
+    SHOW_ALL = 8
+    SHOW_ALL_TO_ALL = 9
+    REVERSE_ORDER = 10
+    CHANGE_POSITION = 11
+
+    def __str__(self) -> str:
+        """Return string representation."""
+        if self == ActionType.NOTHING:
+            return "NOTHING"
+        elif self == ActionType.ASK_DEFENSE:
+            return "ASK_DEFENSE"
+        elif self == ActionType.ASK_EXCHANGE:
+            return "ASK_EXCHANGE"
+        elif self == ActionType.EXCHANGE:
+            return "EXCHANGE"
+        elif self == ActionType.INFECT:
+            return "INFECT"
+        elif self == ActionType.KILL:
+            return "KILL"
+        elif self == ActionType.SHOW:
+            return "SHOW"
+        elif self == ActionType.SHOW_ALL:
+            return "SHOW_ALL"
+        elif self == ActionType.SHOW_ALL_TO_ALL:
+            return "SHOW_ALL_TO_ALL"
+        elif self == ActionType.REVERSE_ORDER:
+            return "REVERSE_ORDER"
+        elif self == ActionType.CHANGE_POSITION:
+            return "CHANGE_POSITION"
+
+
+class GameAction:
+    """
+    Game action class.
+
+    Attributes:
+        Obligatory:
+            action (str): Action (Nothing, Kill).
+            action2 (str): Action (Infect).
+        Optional:
+            target (list): Action targets (player ids).
+            defense_cards (list): List of defense cards.
+            card_target (list): List of cards.
+            exchange_phase (bool): The next phase is exchange.
+
+    Methods:
+        Get methods:
+            get_action(): Return action.
+            get_action2(): Return action2.
+            get_target(): Return targets.
+            get_defense_cards(): Return defense cards.
+            get_card_target(): Return cards target.
+            get_exchange_phase(): Return exchange phase.
+        Set methods:
+            set_action(): Set action.
+            set_target(): Set targets.
+            set_defense_cards(): Set defense cards.
+            set_card_target(): Set cards target.
+            set_exchange_phase(): Set exchange phase.
+    """
+
+    def __init__(
+        self,
+        action: ActionType,
+        action2: Optional[ActionType] = None,
+        target: Optional[list[int]] = None,
+        defense_cards: Optional[list[int]] = None,
+        card_target: Optional[list[int]] = None,
+        exchange_phase: Optional[bool] = None,
+    ):
+        """Initialize class. Argument target is optional."""
+        self.action = action
+        self.action2 = action2
+        self.target = target
+        self.defense_cards = defense_cards
+        self.card_target = card_target
+        self.exchange_phase = exchange_phase
+
+    def __str__(self) -> str:
+        """Return string representation."""
+        return f"Action: {self.action}, Action2: {self.action2}, Target: {self.target}, Defense cards: {self.defense_cards}, Card target: {self.card_target}, Exchange phase: {self.exchange_phase}"
+
+    def get_action(self) -> ActionType:
+        """Return action."""
+        return self.action
+
+    def get_action2(self) -> Optional[ActionType]:
+        """Return action2."""
+        return self.action2
+
+    def get_target(self) -> Optional[list[int]]:
+        """Return target."""
+        return self.target
+
+    def get_defense_cards(self) -> Optional[list[int]]:
+        """Return defense cards."""
+        return self.defense_cards
+
+    def get_card_target(self) -> Optional[list[int]]:
+        """Return cards target."""
+        return self.card_target
+
+    def get_exchange_phase(self) -> Optional[bool]:
+        """Return exchange phase."""
+        if self.exchange_phase is None:
+            return True
+        return self.exchange_phase
+
+    def set_action(self, action: ActionType):
+        """Set action."""
+        self.action = action
+
+    def set_action2(self, action2: Optional[ActionType]):
+        """Set action2."""
+        self.action2 = action2
+
+    def set_target(self, target: Optional[list[int]]):
+        """Set target."""
+        self.target = target
+
+    def set_defense_cards(self, defense_cards: Optional[list[int]]):
+        """Set defense cards."""
+        self.defense_cards = defense_cards
+
+    def set_card_target(self, card_target: Optional[list[int]]):
+        """Set cards target."""
+        self.card_target = card_target
+
+    def set_exchange_phase(self, exchange_phase: Optional[bool]):
+        """Set exchange phase."""
+        self.exchange_phase = exchange_phase
---
+++ src/core/game_logic/game_utility.py
@@ -0,0 +1,159 @@
+"""Functions used to all the game operation (internal logic)."""
+from typing import Optional
+
+from core.game_logic.card import relate_card_with_disposable_deck
+from core.game_logic.card import relate_card_with_player
+from core.game_logic.card import unrelate_card_with_available_deck
+from core.game_logic.card import unrelate_card_with_player
+from core.game_logic.card_creation import init_available_deck
+from core.game_logic.deck import create_available_deck
+from core.game_logic.deck import create_deck
+from core.game_logic.deck import create_disposable_deck
+from core.game_logic.deck import delete_available_deck
+from core.game_logic.deck import delete_deck
+from core.game_logic.deck import delete_disposable_deck
+from core.game_logic.deck import get_deck
+from core.game_logic.deck import get_random_card_from_available_deck
+from core.game_logic.deck import move_disposable_to_available_deck
+from core.game_logic.effects import do_effect
+from core.game_logic.game_action import GameAction
+from models.game import Deck
+from models.game import Game
+from models.game import Player
+from pony.orm import db_session
+
+# ===================== BASIC GAME FUNCTIONS =====================
+
+# Initialization of decks (start of the game)
+
+
+def initialize_decks(id_game: int, quantity_players: int) -> Deck:
+    """Initialize the decks of the game."""
+    create_deck(id_game)
+    create_available_deck(id_game)
+    create_disposable_deck(id_game)
+
+    init_available_deck(id_game, quantity_players)
+
+    return get_deck(id_game)
+
+
+# Delete decks (end of the game)
+
+
+def delete_decks(id_game: int) -> None:
+    """Delete the decks of the game."""
+    delete_deck(id_game)
+    delete_available_deck(id_game)
+    delete_disposable_deck(id_game)
+
+
+# ===================== GAME PHASES FUNCTIONS =====================
+
+# DRAW phase
+
+
+def draw(id_game: int, id_player: int) -> int:
+    """Draw a card from the available deck."""
+    with db_session:
+        if not Player.exists(id=id_player):
+            raise ValueError(f"Player with id {id_player} doesn't exist")
+        if not Game.exists(id=id_game):
+            raise ValueError(f"Game with id {id_game} doesn't exist")
+        if Game[1].current_phase != "Draw":
+            raise ValueError(
+                f"Game with id {id_game} is not in the Draw phase"
+            )
+
+    if len(get_deck(id_game).available_deck.cards) == 0:
+        move_disposable_to_available_deck(id_game)
+
+    card = get_random_card_from_available_deck(id_game)
+    unrelate_card_with_available_deck(card.id, id_game)
+    relate_card_with_player(card.id, id_player)
+    return card.id
+
+
+# PLAY phase
+
+
+def play(
+    id_game: int,
+    id_player: int,
+    idtype_card: int,
+    idtype_card_before: Optional[int] = None,
+    target: Optional[int] = None,
+    card_chosen_by_player: Optional[int] = None,
+    card_chosen_by_target: Optional[int] = None
+) -> GameAction:
+    """Play a card from player hand."""
+    with db_session:
+        if not Game.exists(id=id_game):
+            raise ValueError(f"Game with id {id_game} doesn't exist")
+        if Game[id_game].current_phase not in ["Play", "Defense"]:
+            raise ValueError(
+                f"Game with id {id_game} is not in the Play/Defense phase"
+            )
+        if target is not None and not Player.exists(id=target):
+            raise ValueError(f"Player with id {target} doesn't exist")
+        if not Player.exists(id=id_player):
+            raise ValueError(f"Player with id {id_player} doesn't exist")
+        if idtype_card not in [0, 32]:
+            if len(Player[id_player].hand.select(idtype=idtype_card)) == 0:
+                raise ValueError(
+                    f"Player with id {id_player} has no card with idtype {idtype_card} in hand"
+                )
+        if card_chosen_by_player is not None:
+            if (
+                len(Player[id_player].hand.select(id=card_chosen_by_player))
+                == 0
+            ):
+                raise ValueError(
+                    f"Player with id {id_player} has no card with id {card_chosen_by_player} in hand"
+                )
+        if card_chosen_by_target is not None:
+            if len(Player[target].hand.select(id=card_chosen_by_target)) == 0:
+                raise ValueError(
+                    f"Player with id {target} has no card with id {card_chosen_by_target} in hand"
+                )
+
+    return do_effect(
+        id_game=id_game,
+        id_player=id_player,
+        id_card_type=idtype_card,
+        id_card_type_before=idtype_card_before,
+        target=target,
+        card_chosen_by_player=card_chosen_by_player,
+        card_chosen_by_target=card_chosen_by_target
+    )
+
+
+# DISCARD phase
+
+
+def discard(id_game: int, idtype_card: int, id_player: int) -> int:
+    """Discard a card from player hand."""
+    with db_session:
+        if not Game.exists(id=id_game):
+            raise ValueError(f"Game with id {id_game} doesn't exist")
+        if Game[id_game].current_phase != "Discard":
+            raise ValueError(
+                f"Game with id {id_game} is not in the Discard phase"
+            )
+        if not Player.exists(id=id_player):
+            raise ValueError(f"Player with id {id_player} doesn't exist")
+        if len(Player[id_player].hand) == 0:
+            raise ValueError(
+                f"Player with id {id_player} has no cards in hand"
+            )
+        if len(Player[id_player].hand.select(idtype=idtype_card)) == 0:
+            raise ValueError(
+                f"Player with id {id_player} has no card with idtype {idtype_card} in hand"
+            )
+
+        card = Player[id_player].hand.select(idtype=idtype_card).random(1)[0]
+
+    unrelate_card_with_player(card.id, id_player)
+    relate_card_with_disposable_deck(card.id, id_game)
+
+    return card.id
