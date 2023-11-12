8f32240b8f64af9ecc9ff04eb26cb28995916b74
helcsnewsxd <cphelcsnewsxd@gmail.com>
2023-11-12 14:46:06 -0300

    SAS-358 Implementados tests para el efecto No, gracias

--- tests/game_logic_tests/effects_tests/test_effect_exchange.py
+++ tests/game_logic_tests/effects_tests/test_effect_exchange.py
@@ -1,12 +1,16 @@
 """Test exchange effect."""
+import pytest
+
 from . import clean_db
 from . import commit
 from . import db_session
 from . import Deck
 from . import delete_decks
 from . import discard
+from . import draw_specific
 from . import Game
 from . import initialize_decks
+from . import play
 from . import Player
 
 # ============================ EXCHANGE EFFECT ============================
@@ -126,6 +130,41 @@ class TestNoThanks:
         clean_db()
 
     @db_session
+    def get_player_data(self, id_player: int):
+        """Get player data."""
+        player = Player[id_player]
+        player_data = {
+            "id": player.id,
+            "role": player.role,
+            "name": player.name,
+            "round_position": player.round_position,
+            "alive": player.alive,
+            "game": player.game.id,
+            "hand": [card.id for card in player.hand],
+        }
+
+        return player_data
+
+    @db_session
+    def get_game_data(self, id_game: int):
+        """Get game data."""
+        game = Game[id_game]
+        game_data = {
+            "id": game.id,
+            "round_left_direction": game.round_left_direction,
+            "status": game.status,
+            "current_phase": game.current_phase,
+            "current_position": game.current_position,
+            "winners": game.winners,
+            "players": [
+                self.get_player_data(player.id) for player in game.players
+            ],
+            "deck": game.deck.id,
+        }
+
+        return game_data
+
+    @db_session
     def setup_method(self):
         """Setup method."""
         initialize_decks(id_game=1, quantity_players=12)
@@ -139,7 +178,7 @@ class TestNoThanks:
                 alive=True,
             )
 
-        # draw_specific(id_game=1, id_player=1, idtype_card=1)
+        draw_specific(id_game=1, id_player=2, idtype_card=15)
 
         commit()
 
@@ -147,15 +186,47 @@ class TestNoThanks:
     def teardown_method(self):
         """Teardown method."""
         Game[1].current_phase = "Discard"
-        discard(id_game=1, id_player=1, idtype_card=1)
-        discard(id_game=1, id_player=1, idtype_card=2)
-        discard(id_game=1, id_player=1, idtype_card=3)
+        discard(id_game=1, id_player=2, idtype_card=15)
 
         delete_decks(id_game=1)
         Game[1].delete()
         for i in range(1, 13):
             Player[i].delete()
         commit()
+
+    @db_session
+    def test_no_thanks_effect(self):
+        """Test No Thanks effect."""
+        game_data = self.get_game_data(id_game=1)
+
+        effect = play(
+            id_game=1,
+            attack_player_id=1,
+            defense_player_id=2,
+            idtype_attack_card=32,
+            idtype_defense_card=15,
+        )
+
+        # Without message to front
+        assert effect is None
+
+        # Without modifications in the game and players
+        assert game_data == self.get_game_data(id_game=1)
+
+    @db_session
+    def test_no_thanks_effect_with_dead_player(self):
+        """Test No Thanks effect with dead player."""
+        Player[2].alive = False
+        commit()
+
+        with pytest.raises(ValueError) as excinfo:
+            play(
+                id_game=1,
+                attack_player_id=1,
+                defense_player_id=2,
+                idtype_attack_card=32,
+                idtype_defense_card=15,
+            )
 
 
 # YOU FAILED
