64ab300ac99bf9a3df9012699ccc63fb88609bf9
helcsnewsxd <cphelcsnewsxd@gmail.com>
2023-11-12 14:03:38 -0300

    SAS-344 Realizados tests para casos de error para play()

--- tests/game_logic_tests/test_logic_game_effects.py
+++ tests/game_logic_tests/test_logic_game_effects.py
@@ -1,14 +1,13 @@
 """Test game effects."""
 import pytest
 
-from . import AvailableDeck
-from . import Card
 from . import clean_db
 from . import commit
 from . import db_session
 from . import Deck
-from . import DisposableDeck
-from . import do_effect
+from . import delete_decks
+from . import discard
+from . import draw_specific
 from . import Game
 from . import get_defense_cards
 from . import initialize_decks
@@ -16,11 +15,10 @@ from . import play
 from . import Player
 
 
-class TestPlay:
-    """Test Play Function."""
+class TestPlayInvalidOptions:
+    """Test Play Function with invalid options."""
 
     @classmethod
-    @db_session
     def setup_class(cls):
         """Setup class."""
         clean_db()
@@ -33,152 +31,181 @@ class TestPlay:
     @db_session
     def setup_method(self):
         """Setup method."""
-        Game(id=1, current_phase="Draw")
+        initialize_decks(id_game=1, quantity_players=12)
+        Game(id=1, current_phase="Defense", deck=Deck[1])
         for i in range(1, 13):
-            Player(id=i, name=f"Player{i}", round_position=i, game=Game[1])
-        initialize_decks(1, 12)
-        Game[1].deck = Deck[1]
+            Player(
+                id=i,
+                name=f"Player{i}",
+                round_position=i,
+                game=Game[1],
+                alive=True,
+            )
+
+        draw_specific(id_game=1, id_player=1, idtype_card=1)
+        draw_specific(id_game=1, id_player=1, idtype_card=2)
+        draw_specific(id_game=1, id_player=1, idtype_card=3)
+
         commit()
 
     @db_session
     def teardown_method(self):
         """Teardown method."""
+        Game[1].current_phase = "Discard"
+        discard(id_game=1, id_player=1, idtype_card=1)
+        discard(id_game=1, id_player=1, idtype_card=2)
+        discard(id_game=1, id_player=1, idtype_card=3)
+
+        delete_decks(id_game=1)
         Game[1].delete()
         for i in range(1, 13):
             Player[i].delete()
-        Deck[1].delete()
-        AvailableDeck[1].delete()
-        DisposableDeck[1].delete()
-        for i in range(0, 109):
-            Card[i].delete()
         commit()
 
-    @db_session
-    def test_play(self):
-        """Test play function."""
-        Game[1].current_phase = "Play"
-        for i in range(0, 109):
-            Player[1].hand.add(Card[i])
-
-        for i in range(0, 109):
-            try:
-                action = play(
-                    id_game=1,
-                    id_player=1,
-                    idtype_card=Card[i].idtype,
-                    target=2,
-                )
-                assert str(action) == str(
-                    do_effect(
-                        id_game=1,
-                        id_player=1,
-                        id_card_type=Card[i].idtype,
-                        target=2,
-                    )
-                )
-            except ValueError:
-                with pytest.raises(ValueError):
-                    do_effect(
-                        id_game=1,
-                        id_player=1,
-                        id_card_type=Card[i].idtype,
-                        target=2,
-                    )
-
-    @db_session
-    def test_play_with_invalid_game(self):
-        """Test play function with invalid game."""
-        Game[1].current_phase = "Play"
-
-        with pytest.raises(ValueError):
-            play(id_game=2, id_player=1, idtype_card=Card[0].idtype, target=2)
-
-    @db_session
-    def test_play_with_invalid_phase(self):
-        """Test play function with invalid phase."""
-        Game[1].current_phase = "Draw"
-
-        with pytest.raises(ValueError):
-            play(id_game=1, id_player=1, idtype_card=Card[0].idtype, target=2)
-
-        Game[1].current_phase = "Discard"
-
-        with pytest.raises(ValueError):
-            play(id_game=1, id_player=1, idtype_card=Card[0].idtype, target=2)
-
-    @db_session
-    def test_play_with_invalid_player(self):
-        """Test play function with invalid player."""
-        Game[1].current_phase = "Play"
-
-        with pytest.raises(ValueError):
-            play(id_game=1, id_player=0, idtype_card=Card[0].idtype, target=2)
-
-        with pytest.raises(ValueError):
-            play(id_game=1, id_player=13, idtype_card=Card[0].idtype, target=2)
-
-    @db_session
-    def test_play_with_invalid_card(self):
-        """Test play function with invalid card."""
-        Game[1].current_phase = "Play"
-
-        with pytest.raises(ValueError):
-            play(id_game=1, id_player=1, idtype_card=100, target=2)
-
-    @db_session
-    def test_play_with_invalid_target(self):
-        """Test play function with invalid target."""
-        Game[1].current_phase = "Play"
-
-        with pytest.raises(ValueError):
-            play(id_game=1, id_player=1, idtype_card=Card[0].idtype, target=0)
-
-        with pytest.raises(ValueError):
-            play(id_game=1, id_player=1, idtype_card=Card[0].idtype, target=13)
-
-    @db_session
-    def test_play_with_invalid_card_type_before(self):
-        """Test play function with invalid card_type_before."""
-        Game[1].current_phase = "Play"
-        Player[1].hand.add(Card[0])
+    # Game doesn't exist
+    def test_game_doesnt_exist(self):
+        """Test game doesn't exist."""
 
         with pytest.raises(ValueError):
             play(
-                id_game=1,
-                id_player=1,
-                idtype_card=Card[0].idtype,
-                idtype_card_before=100,
-                target=2,
+                id_game=2,
+                attack_player_id=1,
+                defense_player_id=2,
+                idtype_attack_card=3,
+                idtype_defense_card=0,
             )
 
+    # Game is not in the Defense phase
     @db_session
-    def test_play_with_invalid_card_chosen_by_player(self):
-        """Test play function with invalid card_chosen_by_player."""
+    def test_game_is_not_in_the_defense_phase(self):
+        """Test game is not in the defense phase."""
+
         Game[1].current_phase = "Play"
-        Player[1].hand.add(Card[0])
+        commit()
 
         with pytest.raises(ValueError):
             play(
                 id_game=1,
-                id_player=1,
-                idtype_card=Card[0].idtype,
-                card_chosen_by_player=100,
-                target=2,
+                attack_player_id=1,
+                defense_player_id=2,
+                idtype_attack_card=3,
+                idtype_defense_card=0,
             )
 
-    @db_session
-    def test_play_with_invalid_card_chosen_by_target(self):
-        """Test play function with invalid card_chosen_by_target."""
-        Game[1].current_phase = "Play"
-        Player[1].hand.add(Card[0])
+    # Player doesn't exist
+    def test_player_doesnt_exist(self):
+        """Test player doesn't exist."""
 
         with pytest.raises(ValueError):
             play(
                 id_game=1,
-                id_player=1,
-                idtype_card=Card[0].idtype,
-                card_chosen_by_target=100,
-                target=2,
+                attack_player_id=13,
+                defense_player_id=2,
+                idtype_attack_card=3,
+                idtype_defense_card=0,
+            )
+
+        with pytest.raises(ValueError):
+            play(
+                id_game=1,
+                attack_player_id=1,
+                defense_player_id=13,
+                idtype_attack_card=3,
+                idtype_defense_card=0,
+            )
+
+    # Card is not in hand
+    def test_card_is_not_in_hand(self):
+        """Test card is not in hand."""
+
+        with pytest.raises(ValueError):
+            play(
+                id_game=1,
+                attack_player_id=1,
+                defense_player_id=2,
+                idtype_attack_card=4,
+                idtype_defense_card=0,
+            )
+
+        with pytest.raises(ValueError):
+            play(
+                id_game=1,
+                attack_player_id=1,
+                defense_player_id=2,
+                idtype_attack_card=3,
+                idtype_defense_card=17,
+            )
+
+    # Cards chosen by attacker and defender are not in hand
+    def test_cards_chosen_by_attacker_and_defender_are_not_in_hand(self):
+        """Test cards chosen by attacker and defender are not in hand."""
+
+        with pytest.raises(ValueError):
+            play(
+                id_game=1,
+                attack_player_id=1,
+                defense_player_id=2,
+                idtype_attack_card=32,
+                idtype_defense_card=0,
+                card_chosen_by_attacker=4,
+            )
+
+        with pytest.raises(ValueError):
+            play(
+                id_game=1,
+                attack_player_id=1,
+                defense_player_id=2,
+                idtype_attack_card=32,
+                idtype_defense_card=0,
+                card_chosen_by_defender=4,
+            )
+
+    # The Thing and Infected cards cannot be played
+    def test_the_thing_and_infected_cards_cannot_be_played(self):
+        """Test the thing and infected cards cannot be played."""
+
+        with pytest.raises(ValueError):
+            play(
+                id_game=1,
+                attack_player_id=1,
+                defense_player_id=2,
+                idtype_attack_card=1,
+                idtype_defense_card=0,
+            )
+
+        with pytest.raises(ValueError):
+            play(
+                id_game=1,
+                attack_player_id=1,
+                defense_player_id=2,
+                idtype_attack_card=2,
+                idtype_defense_card=0,
+            )
+
+    # Defense card is not a defense card of the attack card
+    def test_defense_card_is_not_a_defense_card(self):
+        """Test defense card is not a defense card."""
+
+        with pytest.raises(ValueError):
+            play(
+                id_game=1,
+                attack_player_id=1,
+                defense_player_id=2,
+                idtype_attack_card=3,
+                idtype_defense_card=18,
+            )
+
+    # Attack card is not an attack card
+    def test_attack_card_is_not_an_attack_card(self):
+        """Test attack card is not an attack card."""
+
+        with pytest.raises(ValueError):
+            play(
+                id_game=1,
+                attack_player_id=1,
+                defense_player_id=2,
+                idtype_attack_card=18,
+                idtype_defense_card=0,
             )
 
 
