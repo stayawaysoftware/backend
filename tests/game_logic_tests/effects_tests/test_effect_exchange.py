951a8cea4cd960ffac7e4dc3942c0695283feded
helcsnewsxd <cphelcsnewsxd@gmail.com>
2023-11-12 15:16:48 -0300

    SAS-361 Implementados tests para  el efecto Aterrador

--- tests/game_logic_tests/effects_tests/test_effect_exchange.py
+++ tests/game_logic_tests/effects_tests/test_effect_exchange.py
@@ -81,6 +81,41 @@ class TestTerrifying:
         clean_db()
 
     @db_session
+    def get_player_data(self, id_player: int):
+        """Get player data."""
+        player = Player[id_player]
+        player_data = {
+            "id": player.id,
+            "role": player.role,
+            "name": player.name,
+            "round_position": player.round_position,
+            "alive": player.alive,
+            "game": player.game.id,
+            "hand": [card.id for card in player.hand],
+        }
+
+        return player_data
+
+    @db_session
+    def get_game_data(self, id_game: int):
+        """Get game data."""
+        game = Game[id_game]
+        game_data = {
+            "id": game.id,
+            "round_left_direction": game.round_left_direction,
+            "status": game.status,
+            "current_phase": game.current_phase,
+            "current_position": game.current_position,
+            "winners": game.winners,
+            "players": [
+                self.get_player_data(player.id) for player in game.players
+            ],
+            "deck": game.deck.id,
+        }
+
+        return game_data
+
+    @db_session
     def setup_method(self):
         """Setup method."""
         initialize_decks(id_game=1, quantity_players=12)
@@ -94,7 +129,10 @@ class TestTerrifying:
                 alive=True,
             )
 
-        # draw_specific(id_game=1, id_player=1, idtype_card=1)
+        draw_specific(id_game=1, id_player=1, idtype_card=3)
+        draw_specific(id_game=1, id_player=2, idtype_card=4)
+
+        draw_specific(id_game=1, id_player=2, idtype_card=14)
 
         commit()
 
@@ -102,15 +140,72 @@ class TestTerrifying:
     def teardown_method(self):
         """Teardown method."""
         Game[1].current_phase = "Discard"
-        discard(id_game=1, id_player=1, idtype_card=1)
-        discard(id_game=1, id_player=1, idtype_card=2)
         discard(id_game=1, id_player=1, idtype_card=3)
+        discard(id_game=1, id_player=2, idtype_card=4)
+        discard(id_game=1, id_player=2, idtype_card=14)
 
         delete_decks(id_game=1)
         Game[1].delete()
         for i in range(1, 13):
             Player[i].delete()
         commit()
+
+    @db_session
+    def test_terrifying_effect(self):
+        """Test Terrifying effect."""
+        game_data = self.get_game_data(id_game=1)
+
+        effect = play(
+            id_game=1,
+            attack_player_id=1,
+            defense_player_id=2,
+            idtype_attack_card=32,
+            idtype_defense_card=14,
+            card_chosen_by_attacker=3,
+        )
+
+        # Without modifications in the game and players
+        assert game_data == self.get_game_data(id_game=1)
+
+        # With message to front
+        assert effect == {
+            "type": "show_card",
+            "player_name": Player[1].name,
+            "target": [2],
+            "cards": [{"id": 21, "idtype": 3}],
+        }
+
+    @db_session
+    def test_terrifying_effect_with_dead_attaker(self):
+        """Test Terrifying effect with dead attacker."""
+        Player[1].alive = False
+        commit()
+
+        with pytest.raises(ValueError) as excinfo:
+            play(
+                id_game=1,
+                attack_player_id=1,
+                defense_player_id=2,
+                idtype_attack_card=32,
+                idtype_defense_card=14,
+                card_chosen_by_attacker=3,
+            )
+
+    @db_session
+    def test_terrifying_effect_with_dead_defender(self):
+        """Test Terrifying effect with dead defender."""
+        Player[2].alive = False
+        commit()
+
+        with pytest.raises(ValueError) as excinfo:
+            play(
+                id_game=1,
+                attack_player_id=1,
+                defense_player_id=2,
+                idtype_attack_card=32,
+                idtype_defense_card=14,
+                card_chosen_by_attacker=3,
+            )
 
 
 # NO, THANKS
